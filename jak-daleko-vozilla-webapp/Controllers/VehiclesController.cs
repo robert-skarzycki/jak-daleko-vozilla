55aec288c4402bcf41211c418d18808313902dec
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2019-01-09 09:37:02 +0100

    VehiclesController added with fake data

---
+++ jak-daleko-vozilla-webapp/Controllers/VehiclesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using jak_daleko_vozilla_webapp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jak_daleko_vozilla_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    public class VehiclesController : Controller
+    {
+
+        [HttpGet("[action]")]
+        public IEnumerable<Vehicle> NearestVehicles()
+        {
+            return new[] {new Vehicle{
+                PlatesNumber= "DW12345",
+                Distance = 1234.2,
+                SideNumber = "1234"
+            } };
+        }        
+    }
+}
---
+++ jak-daleko-vozilla-webapp/Model/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace jak_daleko_vozilla_webapp.Model
+{
+    public class Vehicle
+    {
+        public string PlatesNumber { get; set; }
+        public string SideNumber { get; set; }
+        public Location Location { get; set; }
+        public double Distance { get; set; }
+    }
+}
